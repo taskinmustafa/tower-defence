b781995f9fa5eb65227bd0d0ee46c18127d4ba00
Mustafa Taşkın <taskin.mmustafa@gmail.com>
2023-03-19 15:31:10 +0300

    basic 3d scene with only camera

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use bevy::prelude::*;
 fn main() {
-    println!("Hello, world!");
+    App::new()
+    .insert_resource(ClearColor(Color::rgb(0.6, 0.2, 0.2)))
+    .add_plugins(DefaultPlugins.set(WindowPlugin{
+        primary_window: Some(Window {
+            resizable: false,
+            ..default()
+        }),
+        ..default()
+    }))
+    .add_startup_system(spawn_camera)
+    .run();
+}
+
+fn spawn_camera(mut commands: Commands) {
+    commands.spawn(
+        Camera3dBundle {
+            transform: Transform::from_xyz(0.0, 0.0, 500.0).looking_at(Vec3::ZERO, Vec3::Y),
+            ..default()
+        }
+    );
 }
