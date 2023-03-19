5a115f766c4ee2348dd9f05fbf9f20ac1a483650
Mustafa Taşkın <taskin.mmustafa@gmail.com>
2023-03-19 15:48:52 +0300

    plane added to scene

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ fn main() {
         ..default()
     }))
     .add_startup_system(spawn_camera)
+    .add_startup_system(spawn_basic_scene)
     .run();
 }
 
@@ -17,6 +18,20 @@ fn spawn_camera(mut commands: Commands) {
     commands.spawn(
         Camera3dBundle {
             transform: Transform::from_xyz(0.0, 0.0, 500.0).looking_at(Vec3::ZERO, Vec3::Y),
+            ..default()
+        }
+    );
+}
+
+fn spawn_basic_scene(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>
+){
+    commands.spawn(
+        PbrBundle{
+            mesh: meshes.add(Mesh::from(shape::Plane{size: 5.0,subdivisions: 8})),
+            material: materials.add(Color::rgb(0.5, 0.1, 0.7).into()),
             ..default()
         }
     );
